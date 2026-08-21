8cfddcf5538865ddcc696347faef54ed8dec4f7d
zacariasct4 <zacariasconde01@gmail.com>
2026-08-21 12:39:04 +0200

    Some corrections to ensure the correct run of all the notebooks

---
+++ src/preprocessing/feature_engineering.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def encode_cyclic_features(df):
+    """
+    La función codifica la dirección del viento, la hora del día y el mes
+    del año mediante transformaciones trigonométricas. Esto permite representar
+    correctamente su carácter periódico, de forma que valores cercanos al inicio
+    y al final de cada ciclo también queden próximos en el espacio transformado.
+    """
+
+    df['direccion_viento_sin'] = np.sin(np.radians(df['direccion_viento']))
+    df['direccion_viento_cos'] = np.cos(np.radians(df['direccion_viento']))
+
+    df["hora_sin"] = np.sin(2 * np.pi * df["hora"] / 24)
+    df["hora_cos"] = np.cos(2 * np.pi * df["hora"] / 24)
+
+    df["mes_sin"] = np.sin(2 * np.pi * (df["mes"] - 1) / 12)
+    df["mes_cos"] = np.cos(2 * np.pi * (df["mes"] - 1) / 12)
+
+    df.drop(columns=['direccion_viento', 'hora', 'mes'], inplace=True)
+    ordered_columns = ['ano', 'mes_sin', 'mes_cos', 'dia', 'hora_sin', 'hora_cos', 'minuto', 'fecha', 'ghi', 'dni', 'dhi',
+       'temperatura', 'velocidad_viento', 'humedad_relativa', 'direccion_viento_sin', 'direccion_viento_cos',
+        'codigo_ghi', 'codigo_dni', 'codigo_dhi']
+    df.colums = ordered_columns
+
+    return df
